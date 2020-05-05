2d262953fabeb569016ac405667779da455481a5
KYLE DOHERTY <kyledoherty@KYLEs-iMac-Pro.local>
2020-05-05 15:27:43 -0700

    host file on github

--- example_bicubic_spline.R
+++ example_bicubic_spline.R
@@ -1,36 +1,36 @@
 library(lidR)
-#source('https://raw.githubusercontent.com/mosscoder/lidR_supplements/master/las_helper_functions.R')
-source('~/mpgPostdoc/projects/bareEarth/analysis/support/las_helper_functions.R')
+source('https://raw.githubusercontent.com/mosscoder/lidR_supplements/master/las_helper_functions.R')
 setwd('~/Downloads')
 
 set_lidr_threads(1L)
-data.table::setDTthreads(1L)
 plan('multisession', workers = 4L)
 
 t <- tempfile()
 
-download.file(url = 'https://storage.googleapis.com/mpg-files/classified_example.las.zip',
+fUrl <- 'https://github.com/mosscoder/lidR_supplements/blob/master/classified_example.las.zip?raw=true'
+
+download.file(url = fUrl,
               destfile = t)
 
 ctg <- catalog(unzip(t)[1])
 
-buffer <- 200
-res <- 0.2
-
-opt_chunk_buffer(ctg) <- buffer
-opt_chunk_size(ctg) <- 100
-opt_stop_early(ctg) <- TRUE
-opt_select(ctg) <- 'xyzc'
-opt_output_files(ctg) <- './bicubic/{XLEFT}_{YBOTTOM}_{ID}'
-
-opt <- list(raster_alignment = res,
-            automerge = TRUE)
-
-dtm <- catalog_apply(ctg, 
-              FUN = bicubicSplineInterp, 
-              buffer = buffer,
-              res = res, 
-              .options = opt)
+  buffer <- 30
+  res <- 0.2
+  
+  opt_chunk_buffer(ctg) <- buffer
+  opt_chunk_size(ctg) <- 200
+  opt_stop_early(ctg) <- TRUE
+  opt_select(ctg) <- 'xyzc'
+  opt_output_files(ctg) <- './bicubic/{XLEFT}_{YBOTTOM}_{ID}'
+  
+  opt <- list(raster_alignment = res,
+              automerge = TRUE)
+  
+  dtm <- catalog_apply(ctg, 
+                FUN = bicubicSplineInterp, 
+                buffer = buffer,
+                res = res, 
+                .options = opt)
 
 hillPlot(readLAS(ctg@data$filename), main = 'TIN interpolation') 
 hillPlot(dtm, main = 'Bicubic spline interpolation') 
